47a24d4c3ae9e95daeae9af52705fa7f6b37008f
Alexandre Uzan <alexandreuzan9@gmail.com>
2025-01-30 13:25:11 +0100

    [in progress]LUKS + LVM Encryption

--- install_arch.sh
+++ install_arch.sh
@@ -41,3 +41,32 @@ echo "Partition racine créée"
 
 parted -s $DISK print # affiche les partitions
 echo "Partitions créées et affichées"
+
+# Formater la partition EFI
+mkfs.fat -F32 "${DISK}1"
+
+### Chiffrement LUKS + LVM ###
+echo "Chiffrement de la partition principale..."
+echo -n "$LUKS_PASS" | cryptsetup luksFormat "${DISK}2" -
+echo "Chiffrement de la partition principale terminé"
+echo -n "$LUKS_PASS" | cryptsetup open "${DISK}2" cryptlvm -
+echo "Ouverture de la partition chiffrée"
+
+# Création des volumes LVM
+echo "Création des volumes LVM..."
+pvcreate /dev/mapper/cryptlvm
+vgcreate vg0 /dev/mapper/cryptlvm
+lvcreate -L 20G vg0 -n root
+lvcreate -L 8G vg0 -n swap
+lvcreate -L 35G vg0 -n home
+lvcreate -L 10G vg0 -n luks_extra
+lvcreate -L 5G vg0 -n shared
+lvcreate -L 2G vg0 -n virtualbox
+
+### Formater les volumes logiques ### 
+echo "Formatage des volumes logiques..."
+mkfs.ext4 /dev/vg0/root
+mkfs.ext4 /dev/vg0/home
+mkfs.ext4 /dev/vg0/shared
+mkfs.ext4 /dev/vg0/virtualbox
+mkswap /dev/vg0/swap
