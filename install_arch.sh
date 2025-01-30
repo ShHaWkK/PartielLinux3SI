8b55ab6aee27692c8916aa4e571ed56730b92f8a
Alexandre Uzan <alexandreuzan9@gmail.com>
2025-01-30 11:43:33 +0100

    [In progress]: Disk partitioning

--- install_arch.sh
+++ install_arch.sh
@@ -22,3 +22,22 @@ echo "Installation d'Arch Linux"
 sleep 2
 
 ### Partitionnement du disque ###
+echo "### Création des partitions ###"
+
+# Création du label GPT
+parted -s $DISK mklabel gpt 
+echo "Label GPT créé sur $DISK"
+
+
+# partition de boot ESP
+parted -s $DISK mkpart primary fat32 1MiB 513MiB 
+echo "Partition de boot créée"
+
+parted -s $DISK set 1 esp on 
+echo "Partition de boot définie comme ESP"
+# partition racine chiffrée
+parted -s $DISK mkpart primary ext4 513MiB 100%
+echo "Partition racine créée"
+
+parted -s $DISK print # affiche les partitions
+echo "Partitions créées et affichées"
